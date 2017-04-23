c2a8f3128f812e9ad2803bf1fe58d4b9d1ae0b53
Libbie <eaharris2119@gmail.com>
2017-04-23 15:11:37 -0500

    Uploaded results of programming assignment

---
+++ plot1.R
@@ -0,0 +1,18 @@
+## Read in Data
+power <- read.table("household_power_consumption.txt",skip=1,sep=";")
+names(power) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+subpower <- subset(power,power$Date=="1/2/2007" | power$Date =="2/2/2007")
+
+## Transform Dates and Time
+subpower$Date <- as.Date(subpower$Date, format="%d/%m/%Y")
+subpower$Time <- strptime(subpower$Time, format="%H:%M:%S")
+subpower[1:1440,"Time"] <- format(subpower[1:1440,"Time"],"2007-02-01 %H:%M:%S")
+subpower[1441:2880,"Time"] <- format(subpower[1441:2880,"Time"],"2007-02-02 %H:%M:%S")
+
+## Create and Annotate Histogram
+hist(as.numeric(as.character(subpower$Global_active_power)),col="red",
+     main="Global Active Power",xlab="Global Active Power(kilowatts)")
+
+## Copy to PNG
+dev.copy(png, file = "plot1.png")
+dev.off()
